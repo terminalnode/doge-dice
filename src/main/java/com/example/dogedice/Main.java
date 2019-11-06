49cce158a0c9ef73f958b082e06eca8c991215d6
Alexander <2683344+terminalnode@users.noreply.github.com>
2019-11-06 14:02:34 +0100

    removed unnecessary code

--- src/main/java/com/example/dogedice/Main.java
+++ src/main/java/com/example/dogedice/Main.java
@@ -3,97 +3,79 @@ package com.example.dogedice;
 import javafx.application.*;
 import javafx.geometry.Insets;
 import javafx.scene.*;
+import javafx.scene.layout.*;
 import javafx.stage.*;
 import javafx.scene.control.*;
-import javafx.scene.layout.BorderPane;
 import javafx.scene.image.Image;
 import javafx.scene.image.ImageView;
 import java.io.FileInputStream;
 import java.io.FileNotFoundException;
+
+import static javax.swing.text.StyleConstants.Background;
 
 public class Main extends Application {
   public static void main(String[] args) {
     // Start the JavaFX application by calling launch().
     launch(args);
   }
+
   // Override the start() method.
-  public void start(Stage myStage) throws FileNotFoundException{
-    myStage.setResizable(false);
+  public void start(Stage mainWindow) throws FileNotFoundException{
+    mainWindow.setResizable(false);
+
     // DogeSpin
     Image image = new Image (new FileInputStream("res/dogespin.gif"));
     ImageView dogeSpin = new ImageView(image);
-    dogeSpin.setX(10);
-    dogeSpin.setY(10);
+    ImageView dogeSpin2 = new ImageView(image);
     dogeSpin.setPreserveRatio(true);
-    dogeSpin.setFitWidth(30);
-    //dogeSpin.setFitHeight(30);
-
-    //DogeSpin2
-    Image image2 = new Image (new FileInputStream("res/dogespin.gif"));
-    ImageView dogeSpin2 = new ImageView(image2);
-    dogeSpin2.setX(10);
-    dogeSpin2.setY(10);
-    dogeSpin2.setFitWidth(30);
-    dogeSpin2.setFitHeight(30);
     dogeSpin2.setPreserveRatio(true);
-
+    dogeSpin.setFitWidth(30);
+    dogeSpin2.setFitWidth(30);
 
     //Dogelogo
     Image image1 = new Image (new FileInputStream("res/dogelogo.png"));
     ImageView dogeLogo = new ImageView(image1);
-    dogeLogo.setFitWidth(365);
-    dogeLogo.setFitHeight(271);
     dogeLogo.setPreserveRatio(true);
+    dogeLogo.setFitWidth(365);
 
     //DogeCoin
     Image image3 = new Image (new FileInputStream("res/dogecoin.png"));
     ImageView dogeCoin = new ImageView(image3);
-    dogeCoin.setFitWidth(25);
-    dogeCoin.setFitHeight(25);
     dogeCoin.setPreserveRatio(true);
+    dogeCoin.setFitWidth(25);
     dogeCoin.setLayoutX(440);
-
 
     //Help
     Image image4 = new Image (new FileInputStream("res/help.png"));
     ImageView help = new ImageView(image4);
-    help.setFitWidth(25);
-    help.setFitHeight(25);
     help.setPreserveRatio(true);
+    help.setFitWidth(25);
     help.setLayoutX(100);
 
-    Group group2 = new Group();
-    group2.getChildren().addAll(dogeCoin, help);
-
+    Group helpCoinGroup = new Group();
+    helpCoinGroup.getChildren().addAll(dogeCoin, help);
 
     Button btnStart = new Button("START");
     Button btnHighScore = new Button ("HIGHSCORE");
     btnStart.setLayoutX(50);
-    btnHighScore.setLayoutX(110);
+    btnHighScore.setLayoutX(150);
 
-    Group group1 = new Group();
-    group1.getChildren().addAll(btnStart, btnHighScore);
+    Group buttonGroup = new Group();
+    buttonGroup.getChildren().addAll(btnStart, btnHighScore);
 
     BorderPane bPane = new BorderPane();
     bPane.setPadding(new Insets(5,5,5,5));
     bPane.setTop(dogeLogo);
-    bPane.setBottom(group2);
+    bPane.setBottom(helpCoinGroup);
+    bPane.setCenter(buttonGroup);
     bPane.setLeft(dogeSpin);
     bPane.setRight(dogeSpin2);
-    bPane.setCenter(group1);
-
-
 
     Scene myScene = new Scene(bPane);
 
-    // Give the stage a title.
-    myStage.setTitle("Dogedice");
-
-    // Set the scene on the stage.
-    myStage.setScene(myScene);
-
-    // Show the stage and its scene.
-    myStage.show();
+    mainWindow.setTitle("Dogedice");  // Give the stage a title.
+    mainWindow.setScene(myScene);     // Set the scene on the stage.
+    mainWindow.show();                // Show the stage and its scene.
   }
 }
 
