08754a30cce02496bcf35a2b11bdd312841f4dfa
Alexander <2683344+terminalnode@users.noreply.github.com>
2019-11-09 05:13:59 +0100

    helpWindow now looks good and can handle linebreaks

--- src/main/java/com/example/dogedice/controllers/HelpWindow.java
+++ src/main/java/com/example/dogedice/controllers/HelpWindow.java
@@ -1,4 +1,17 @@
 package com.example.dogedice.controllers;
 
+import javafx.fxml.FXML;
+import javafx.scene.control.Label;
+
 public class HelpWindow {
+  @FXML
+  Label text;
+
+  @FXML
+  public void initialize() {
+    text.setText(
+        "1. Happy fun for whole family! :D\n" +
+            "2. Enjoy! >:("
+    );
+  }
 }
