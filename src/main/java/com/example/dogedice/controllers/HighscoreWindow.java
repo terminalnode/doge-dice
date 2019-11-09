7029a3d9ab41a3ea80072eeb3373b8e7f130b4b6
hamnord <h.nordenstein@hotmail.com>
2019-11-10 00:18:49 +0100

    added and modified Highscore Controller and Icons

--- src/main/java/com/example/dogedice/controllers/HighscoreWindow.java
+++ src/main/java/com/example/dogedice/controllers/HighscoreWindow.java
@@ -1,4 +1,23 @@
 package com.example.dogedice.controllers;
 
+import javafx.scene.image.ImageView;
+import javafx.scene.input.MouseEvent;
+
 public class HighscoreWindow {
+/*
+  public void highscoreRanking () {
+  }*/
+
+  public void spinningDogeClicked(MouseEvent mouseEvent)  {
+    ImageView spinningDoge = (ImageView ) mouseEvent.getSource();
+    spinningDoge.setScaleX(spinningDoge.getScaleX() * -1);
+
+  }
+/*
+  public void backIconClicked(MouseEvent mouseEvent) throws IOException {
+    //ImageView backIcon = (ImageView) mouseEvent.getSource();
+    //HighscoreWindow.openWindow("fxml/mainWindow.fxml", "Main Window", mouseEvent);
+  }
+*/
 }
+
