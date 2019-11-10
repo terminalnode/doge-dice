67a240120b3993c309801b45e187771b6e6abebd
Alexander <2683344+terminalnode@users.noreply.github.com>
2019-11-10 04:03:23 +0100

    working back-button in highscorewindow

--- src/main/java/com/example/dogedice/controllers/HighscoreWindow.java
+++ src/main/java/com/example/dogedice/controllers/HighscoreWindow.java
@@ -1,23 +1,20 @@
 package com.example.dogedice.controllers;
 
+import com.example.dogedice.Main;
 import javafx.scene.image.ImageView;
 import javafx.scene.input.MouseEvent;
 
+import java.io.IOException;
+
 public class HighscoreWindow {
-/*
-  public void highscoreRanking () {
-  }*/
 
   public void spinningDogeClicked(MouseEvent mouseEvent)  {
     ImageView spinningDoge = (ImageView ) mouseEvent.getSource();
     spinningDoge.setScaleX(spinningDoge.getScaleX() * -1);
+  }
 
-  }
-/*
   public void backIconClicked(MouseEvent mouseEvent) throws IOException {
-    //ImageView backIcon = (ImageView) mouseEvent.getSource();
-    //HighscoreWindow.openWindow("fxml/mainWindow.fxml", "Main Window", mouseEvent);
+    Main.replaceWindow("fxml/mainWindow.fxml", "Main Window", mouseEvent);
   }
-*/
 }
 
