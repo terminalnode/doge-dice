96e33bdbe2dc29371fdb5e4eacafe90e39400b40
Alexander <2683344+terminalnode@users.noreply.github.com>
2019-11-10 04:27:56 +0100

    fixed NamePlayersWindow

--- src/main/java/com/example/dogedice/controllers/NamePlayersWindow.java
+++ src/main/java/com/example/dogedice/controllers/NamePlayersWindow.java
@@ -1,11 +1,10 @@
 package com.example.dogedice.controllers;
 
 import javafx.fxml.FXML;
-import javafx.geometry.Orientation;
+import javafx.scene.Group;
 import javafx.scene.control.Label;
 import javafx.scene.control.TextField;
 import javafx.scene.input.MouseEvent;
-import javafx.scene.layout.FlowPane;
 import javafx.scene.layout.VBox;
 
 public class NamePlayersWindow {
@@ -14,23 +13,25 @@ public class NamePlayersWindow {
 
   void addHumPlayer(int num) {
     Label label = new Label("Human: ");
+    label.setLayoutY(5);
     TextField textField = new TextField(String.format("Player #%s", num));
     textField.prefWidth(50);
+    textField.setLayoutX(70);
 
-    FlowPane parent = new FlowPane();
-    parent.setOrientation(Orientation.HORIZONTAL);
+    Group parent = new Group();
     parent.getChildren().addAll(label, textField);
     namePlayersBox.getChildren().add(parent);
   }
 
   void addCpuPlayer(int num) {
     Label label = new Label("CPU: ");
+    label.setLayoutY(5);
     TextField textField = new TextField(String.format("CPU #%s", num));
     textField.prefWidth(50);
+    textField.setLayoutX(70);
     textField.setEditable(false);
 
-    FlowPane parent = new FlowPane();
-    parent.setOrientation(Orientation.HORIZONTAL);
+    Group parent = new Group();
     parent.getChildren().addAll(label, textField);
     namePlayersBox.getChildren().add(parent);
   }
