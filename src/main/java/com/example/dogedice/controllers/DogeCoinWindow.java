02aca8e03f3c4ad587920cc9a8ec95484476a81b
Herman Singh <hermansingh1993@gmail.com>
2019-11-11 09:33:08 +0100

    Added backbutton to help and dogecoinwindow

--- src/main/java/com/example/dogedice/controllers/DogeCoinWindow.java
+++ src/main/java/com/example/dogedice/controllers/DogeCoinWindow.java
@@ -1,4 +1,14 @@
 package com.example.dogedice.controllers;
 
+import com.example.dogedice.Main;
+import javafx.scene.input.MouseEvent;
+
+import java.io.IOException;
+
 public class DogeCoinWindow {
+
+  public void backIconClicked(MouseEvent mouseEvent) throws IOException {
+    Main.replaceWindow("fxml/mainWindow.fxml", "Main Window", mouseEvent);
+  }
 }
+
--- src/main/java/com/example/dogedice/controllers/HelpWindow.java
+++ src/main/java/com/example/dogedice/controllers/HelpWindow.java
@@ -1,11 +1,18 @@
 package com.example.dogedice.controllers;
 
+import com.example.dogedice.Main;
 import javafx.fxml.FXML;
 import javafx.scene.control.Label;
+import javafx.scene.input.MouseEvent;
+
+import java.io.IOException;
 
 public class HelpWindow {
   @FXML
   Label text;
+  public void backIconClicked(MouseEvent mouseEvent) throws IOException {
+    Main.replaceWindow("fxml/mainWindow.fxml", "Main Window", mouseEvent);
+  }
 
   @FXML
   public void initialize() {
