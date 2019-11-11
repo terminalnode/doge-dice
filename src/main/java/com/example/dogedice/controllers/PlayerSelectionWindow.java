3322ea883d486c1572caab2a8e91e32079d22fa2
Alexander <2683344+terminalnode@users.noreply.github.com>
2019-11-11 10:46:33 +0100

    PlayerSelectionWindow will now deny starting games with fewer than two players

--- src/main/java/com/example/dogedice/controllers/PlayerSelectionWindow.java
+++ src/main/java/com/example/dogedice/controllers/PlayerSelectionWindow.java
@@ -5,9 +5,13 @@ import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
+import javafx.scene.control.Alert;
+import javafx.scene.control.Alert.AlertType;
+import javafx.scene.control.ButtonType;
 import javafx.scene.control.Spinner;
 import javafx.scene.image.ImageView;
 import javafx.scene.input.MouseEvent;
+import javafx.scene.layout.Region;
 import javafx.stage.Stage;
 
 import java.io.IOException;
@@ -17,23 +21,27 @@ public class PlayerSelectionWindow {
   Spinner<Integer> humPlayersSpinner, cpuPlayersSpinner;
 
   public void confirmButtonClicked(MouseEvent mouseEvent) throws IOException {
-    Stage newWindow = new Stage();
-    newWindow.setTitle("Name Players");
-    FXMLLoader loader = Main.getLoader("fxml/namePlayersWindow.fxml");
-    Parent root = loader.load();
-    NamePlayersWindow controller = loader.getController();
-    Scene scene = new Scene(root);
-    newWindow.setScene(scene);
-    for (int i = 1; i <= humPlayersSpinner.getValue(); i++) {
-      controller.addHumPlayer(i);
-    }
+    int numHum = humPlayersSpinner.getValue();
+    int numCpu = cpuPlayersSpinner.getValue();
 
-    for (int i = 1; i <= cpuPlayersSpinner.getValue(); i++) {
-      controller.addCpuPlayer(i);
-    }
+    if (numCpu + numHum < 2) {
+      Alert alert = new Alert(AlertType.NONE, "Total number of players must be at least 2.", ButtonType.OK);
+      alert.setTitle("Too few players!");
+      alert.show();
+    } else {
+      Stage newWindow = new Stage();
+      newWindow.setTitle("Name Players");
+      FXMLLoader loader = Main.getLoader("fxml/namePlayersWindow.fxml");
+      Parent root = loader.load();
+      NamePlayersWindow controller = loader.getController();
+      Scene scene = new Scene(root);
+      newWindow.setScene(scene);
+      for (int i = 1; i <= numHum; i++) { controller.addHumPlayer(i); }
+      for (int i = 1; i <= numCpu; i++) { controller.addCpuPlayer(i); }
 
-    Main.hideParentWindow(mouseEvent);
-    newWindow.show();
+      Main.hideParentWindow(mouseEvent);
+      newWindow.show();
+    }
   }
 
   void setHumPlayersSpinner(int num) {
