44ecd5adb924edf38c7890590061a240055a95d5
Herman Singh <hermansingh1993@gmail.com>
2019-11-12 09:32:47 +0100

    Added feature to go back to mainWindow from playWindow + cleaned upp playWindow.css

--- src/main/java/com/example/dogedice/controllers/PlayWindow.java
+++ src/main/java/com/example/dogedice/controllers/PlayWindow.java
@@ -1,4 +1,24 @@
 package com.example.dogedice.controllers;
 
+import com.example.dogedice.Main;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.scene.control.Alert;
+import javafx.scene.control.ButtonType;
+import javafx.scene.input.MouseEvent;
+
+import java.io.IOException;
+
 public class PlayWindow {
+  public void backIconClicked(MouseEvent mouseEvent) throws IOException {
+    FXMLLoader loader = Main.getLoader("fxml/mainWindow.fxml");
+    Parent root = loader.load();
+
+    Scene scene = new Scene(root);
+
+    Main.replaceStage(mouseEvent, scene, "Doge dice");
+
+
+  }
 }
