09f43f5dc30b153ccdf1edbc344a0707b2b1294d
hamnord <h.nordenstein@hotmail.com>
2019-11-13 13:11:47 +0100

    modified printwinner function

--- src/main/java/com/example/dogedice/controllers/WinnerWindow.java
+++ src/main/java/com/example/dogedice/controllers/WinnerWindow.java
@@ -2,20 +2,31 @@ package com.example.dogedice.controllers;
 
 import com.example.dogedice.Main;
 import javafx.fxml.FXML;
+import javafx.scene.Group;
+import javafx.scene.Parent;
 import javafx.scene.image.ImageView;
 import javafx.scene.input.MouseEvent;
 import java.io.IOException;
 import javafx.scene.control.Label;
+import javafx.scene.layout.HBox;
+import javafx.scene.layout.VBox;
 
 public class WinnerWindow {
 
    @FXML
-   Label text;
+   VBox winnerBox;
 
-   @FXML
-  public void printWinner() {
-   text.setText("FIttJOCKEE");
-  }
+
+  private void printWinner(String listWinner) {
+     Label label = new Label(listWinner + " \n Jocke");
+    label.setLayoutY(50);
+    label.setPrefHeight(50);
+    label.setPrefWidth(50);
+
+     winnerBox
+         .getChildren()
+         .add(label);
+    }
 
   public void menuButtonClicked(MouseEvent mouseEvent) throws IOException {
     Main.replaceWindow("fxml/mainWindow.fxml", "Main Window", mouseEvent);
