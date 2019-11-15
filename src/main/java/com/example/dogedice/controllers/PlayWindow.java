6d41cb682fec02182f5b7426dcf391cf2e1f7270
Alexander <2683344+terminalnode@users.noreply.github.com>
2019-11-15 11:24:29 +0100

    added hashmaps for storing various playerinfo

--- src/main/java/com/example/dogedice/controllers/PlayWindow.java
+++ src/main/java/com/example/dogedice/controllers/PlayWindow.java
@@ -1,41 +1,46 @@
 package com.example.dogedice.controllers;
 
-import com.example.dogedice.model.Die;
-import com.example.dogedice.model.HumanPlayer;
 import com.example.dogedice.model.Player;
 import javafx.fxml.FXML;
-import javafx.fxml.FXMLLoader;
-import javafx.scene.Parent;
-import javafx.scene.Scene;
 import javafx.scene.control.*;
 import javafx.scene.input.MouseEvent;
+import javafx.scene.layout.FlowPane;
+import javafx.scene.layout.HBox;
 import javafx.scene.layout.VBox;
 
 import java.io.IOException;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 public class PlayWindow {
   private int index = 0;
   private List<Player> players;
+  private final Map<Player, Label> playerNames;
+  private final Map<Player, Label> playerScores;
+  private final Map<Player, FlowPane> playerDice;
+
+  public PlayWindow() {
+    playerNames = new HashMap<>();  // the labels where we display player names
+    playerScores = new HashMap<>(); // the labels where we display player scores
+    playerDice = new HashMap<>();   // the flowpane where we display player dice/modifiers
+  }
 
   @FXML
   VBox playerPaneBox;
 
-  public void backButtonClicked(MouseEvent mouseEvent) throws IOException {
-    FXMLLoader loader = HelperMethods.getLoader(HelperMethods.mainWindowFXML);
-    Parent root = loader.load();
-    Scene scene = new Scene(root);
-    HelperMethods.replaceWindow(HelperMethods.mainWindowFXML, HelperMethods.mainWindowTitle, mouseEvent);  }
-
   @FXML
   Label roll;
+
+  public void backButtonClicked(MouseEvent mouseEvent) throws IOException {
+    HelperMethods.replaceWindow(HelperMethods.mainWindowFXML, HelperMethods.mainWindowTitle, mouseEvent);
+  }
 
   public void rollButtonClicked(MouseEvent mouseEvent) throws IOException{
     Player player = players.get(index);
       roll.setText("" + player.rollAllDice());
       changePlayer();
       addScore();
-
   }
 
   @FXML
@@ -47,13 +52,22 @@ public class PlayWindow {
 
 
   public void addPlayers(List<Player> players) {
+    this.players = players;
+
     for (Player player : players){
-      Label label = new Label(player.getName());
+      playerNames.put(player, new Label(player.getName()));
+      playerScores.put(player, new Label("" + player.getScore()));
+      playerDice.put(player, new FlowPane());
+      HBox playerInfo = new HBox();
+      playerInfo.getChildren().addAll(playerNames.get(player), playerScores.get(player));
+
       playerPaneBox
         .getChildren()
-          .add(label);
+          .addAll(
+              playerInfo,
+              playerDice.get(player)
+          );
     }
-    this.players = players;
   }
 
 
