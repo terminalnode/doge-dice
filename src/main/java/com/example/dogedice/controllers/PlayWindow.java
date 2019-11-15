a1a00dce7f49eec834fd7399d716a1f3d0f231b5
Herman Singh <hermansingh1993@gmail.com>
2019-11-15 16:13:58 +0100

    Added InactiveStyle and Activestyle for players

--- src/main/java/com/example/dogedice/controllers/PlayWindow.java
+++ src/main/java/com/example/dogedice/controllers/PlayWindow.java
@@ -36,22 +36,39 @@ public class PlayWindow {
 
   public void rollButtonClicked(MouseEvent mouseEvent) {
     Player player = players.get(index);
-      roll.setText("" + player.rollAllDice());
-      changePlayer();
+    roll.setText("" + player.rollAllDice());
+    playerScores.get(player).setText(""+player.getScore());
+    changePlayer();
+
+    Player nextPlayer = players.get(index);
+    if (nextPlayer.isBot()) {
+      rollButtonClicked(null);
+    }
+    setInactiveStylePlayer(player);
+    setActiveStylePlayer(nextPlayer);
   }
+
+  public void setActiveStylePlayer(Player player) {
+    playerNames.get(player).setStyle("-fx-font: bold italic 20pt 'Arial'; -fx-border-color:BLUE ");
+  }
+
+  public void setInactiveStylePlayer(Player player) {
+    playerNames.get(player).setStyle("-fx-border-color: transparent");
+  }
+
 
   public void addPlayers(List<Player> players) {
     this.players = players;
 
     for (Player player : players){
       playerNames.put(player, new Label(player.getName()));
-      playerScores.put(player, new Label("" + player.getScore()));
+      playerScores.put(player, new Label(""  + player.getScore()));
       playerItems.put(player, new FlowPane());
       GridPane playerInfo = new GridPane();
       playerInfo.setHgap(30);
       playerInfo.addColumn(0 , playerNames.get(player));
       playerInfo.addColumn(1, playerScores.get(player));
-     // playerScores.get(player).setText("" +player.getScore());
+      playerScores.get(player).setText("" +player.getScore());
       playerInfo.getColumnConstraints().addAll(new ColumnConstraints(300), new ColumnConstraints(50));
       playerPaneBox
         .getChildren()
