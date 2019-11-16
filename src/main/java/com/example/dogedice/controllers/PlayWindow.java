4a4cb0dde4fb1dca58540b4e84d9f8b2ded2c11a
Alexander <2683344+terminalnode@users.noreply.github.com>
2019-11-16 12:18:23 +0100

    separated most styling in playWindow to css

--- src/main/java/com/example/dogedice/controllers/PlayWindow.java
+++ src/main/java/com/example/dogedice/controllers/PlayWindow.java
@@ -18,17 +18,17 @@ public class PlayWindow {
   private final Map<Player, Label> playerScores;
   private final Map<Player, FlowPane> playerItems;
 
-  public PlayWindow() {
-    playerNames = new HashMap<>();  // the labels where we display player names
-    playerScores = new HashMap<>(); // the labels where we display player scores
-    playerItems = new HashMap<>();   // the flowpane where we display player dice/modifiers
-  }
-
   @FXML
   VBox playerPaneBox;
 
   @FXML
   Label roll;
+
+  public PlayWindow() {
+    playerNames = new HashMap<>();  // the labels where we display player names
+    playerScores = new HashMap<>(); // the labels where we display player scores
+    playerItems = new HashMap<>();  // the flowpane where we display player dice/modifiers
+  }
 
   public void dieTwentyClicked(MouseEvent mouseEvent) {
 
@@ -60,22 +60,18 @@ public class PlayWindow {
     }
   }
 
-  public void setActiveStylePlayer(Player player) {
-    playerNames.get(player).setStyle("-fx-font: bold italic 16pt 'Arial';" +
-        " -fx-border-color:#2196F3;" +
-        " -fx-text-fill: #2196F3;");
-  }
-
-  public void setInactiveStylePlayer(Player player) {
-    playerNames.get(player).setStyle("-fx-border-color: transparent");
-  }
-
-  public void addPlayers(List<Player> players) {
+  void addPlayers(List<Player> players) {
     this.players = players;
 
     for (Player player : players){
-      playerNames.put(player, new Label(player.getName()));
-      playerScores.put(player, new Label(""  + player.getScore()));
+      Label playerNameLabel = new Label(player.getName());
+      playerNameLabel.getStyleClass().add("playerInfo");
+      playerNames.put(player, playerNameLabel);
+
+      Label playerScore = new Label("" + player.getScore());
+      playerScore.getStyleClass().add("playerInfo");
+      playerScores.put(player, playerScore);
+
       playerItems.put(player, new FlowPane());
       GridPane playerInfo = new GridPane();
       playerInfo.setHgap(30);
@@ -90,12 +86,21 @@ public class PlayWindow {
               playerItems.get(player)
           );
     }
+    setActiveStylePlayer(this.players.get(0));
   }
 
-  public void changePlayer() {
+  private void setActiveStylePlayer(Player player) {
+    playerNames.get(player).setStyle("-fx-text-fill: #2196F3;");
+  }
+
+  private void setInactiveStylePlayer(Player player) {
+    playerNames.get(player).setStyle("");
+  }
+
+  private void changePlayer() {
     index++;
     if (players.size() == index){
-     index = 0;
+      index = 0;
     }
   }
 }
