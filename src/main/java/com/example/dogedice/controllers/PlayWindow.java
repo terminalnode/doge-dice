1dd74592ad0a5238a4473787cd81a9621d4840d6
Alexander <2683344+terminalnode@users.noreply.github.com>
2019-11-19 13:31:12 +0100

    refactored PlayWindow a bit

--- src/main/java/com/example/dogedice/controllers/PlayWindow.java
+++ src/main/java/com/example/dogedice/controllers/PlayWindow.java
@@ -14,14 +14,13 @@ import javafx.scene.shape.SVGPath;
 import org.apache.commons.io.IOUtils;
 
 import java.io.IOException;
-import java.net.URISyntaxException;
 import java.nio.charset.StandardCharsets;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
+import java.util.Objects;
 
 public class PlayWindow extends GenericController {
-  private int index = 0;
   private List<Player> players;
   private final Map<Player, Label> playerNames;
   private final Map<Player, Label> playerScores;
@@ -55,7 +54,7 @@ public class PlayWindow extends GenericController {
   public void rollButtonClicked(MouseEvent mouseEvent) throws IOException {
     Player player = gameEngine.getPlayer();
 
-    roll.setText("" + player.rollAllDice());
+    roll.setText("" + gameEngine.rollDice());
     playerScores.get(player).setText(gameEngine.getScoreAsString());
 
     gameEngine.incrementPlayer();
@@ -69,11 +68,9 @@ public class PlayWindow extends GenericController {
     System.out.println(gameTurns);
     gameTurns.setText("Rounds Left: " + gameEngine.getRoundsLeftAsString());
     getWinner(mouseEvent);
-
   }
 
   private void getWinner(MouseEvent mouseEvent) throws IOException {
-
     if (gameEngine.getRoundsLeft() == 0) {
         HelperMethods.replaceScene(
             HelperMethods.winnerWindowFXML,
@@ -81,82 +78,80 @@ public class PlayWindow extends GenericController {
             mouseEvent,
             gameEngine);
       }
-
-  }
-
-  private void pricing(){
-    Group priceGroup = new Group();
-    Label dieSixPrice = new Label(gameEngine.getD6PriceAsString());
-    dieSixPrice.setId("d6p");
-    Label dieTwentyPrice = new Label (gameEngine.getD20PriceAsString());
-    dieTwentyPrice.setId("d20p");
-    Label modifierPrice = new Label (gameEngine.getModifierPriceAsString());
-    modifierPrice.setId("mp");
-    priceGroup.getStylesheets().add("css/playWindow.css");
-    priceGroup.setId("prices");
-    priceGroup.getChildren().addAll(dieSixPrice, dieTwentyPrice, modifierPrice);
-    diceBox.getChildren().add(priceGroup);
   }
 
   @Override
   public void postInitialization() {
-    pricing();
+    Label dieSixPrice = new Label (gameEngine.getD6PriceAsString());
+    Label dieTwentyPrice = new Label (gameEngine.getD20PriceAsString());
+    Label modifierPrice = new Label (gameEngine.getModifierPriceAsString());
+
+    dieSixPrice.setId("d6p");
+    dieTwentyPrice.setId("d20p");
+    modifierPrice.setId("mp");
+
+    Group priceGroup = new Group(dieSixPrice, dieTwentyPrice, modifierPrice);
+    priceGroup.getStylesheets().add("css/playWindow.css");
+    priceGroup.setId("prices");
+    diceBox.getChildren().add(priceGroup);
+
+    SVGPath die6 = null;
+    SVGPath die20 = null;
+    SVGPath modifier = null;
+
     gameTurns.setText("Rounds Left: " + gameEngine.getRoundsLeftAsString());
-    try{
-      SVGPath die6 = getSVGIcon("svgpaths/d6");
+    try {
+      die6 = getSVGIcon("svgpaths/d6");
       resizeSVG(die6, 60, 60 );
       Group d6G = new Group(die6);
 
-      SVGPath die20 = getSVGIcon("svgpaths/d20");
+      die20 = getSVGIcon("svgpaths/d20");
       resizeSVG(die20, 60, 65);
       Group d20G = new Group(die20);
 
-      SVGPath modifier = getSVGIcon("svgpaths/modifier");
+      modifier = getSVGIcon("svgpaths/modifier");
       resizeSVG(modifier, 60, 60);
       Group modG = new Group(modifier);
       diceBox.getChildren().addAll(d6G, d20G, modG);
+    } catch (Exception e) {
+      e.printStackTrace();
+    }
 
-      die6.setOnMousePressed(mouseEvent -> {
+      Objects.requireNonNull(die6).setOnMousePressed(mouseEvent -> {
         Die newDie = gameEngine.buyD6();
         if (newDie != null){
           try {
-            playerItems.get(gameEngine.getPlayer()).getChildren().add(getDieIcon(newDie));
-          } catch (IOException e) { e.printStackTrace();
-          } catch (URISyntaxException e) { e.printStackTrace(); }
+            playerItems.get(gameEngine.getPlayer()).getChildren().add(getIcon(newDie));
+          } catch (Exception e) {
+            e.printStackTrace();
+          }
         }
-
         playerScores.get(gameEngine.getPlayer()).setText(gameEngine.getScoreAsString());
-
       });
 
-      die20.setOnMousePressed(mouseEvent -> {
+      Objects.requireNonNull(die20).setOnMousePressed(mouseEvent -> {
         Die newDie = gameEngine.buyD20();
         if (newDie != null){
           try {
-            playerItems.get(gameEngine.getPlayer()).getChildren().add(getDieIcon(newDie));
-          } catch (IOException e) { e.printStackTrace();
-          } catch (URISyntaxException e) { e.printStackTrace();
+            playerItems.get(gameEngine.getPlayer()).getChildren().add(getIcon(newDie));
+          } catch (Exception e) {
+            e.printStackTrace();
           }
         }
-
         playerScores.get(gameEngine.getPlayer()).setText(gameEngine.getScoreAsString());
       });
 
-      modifier.setOnMousePressed(mouseEvent -> {
+      Objects.requireNonNull(modifier).setOnMousePressed(mouseEvent -> {
         Modifier newModifier = gameEngine.buyModifier();
         if(newModifier != null){
           try {
-            playerItems.get(gameEngine.getPlayer()).getChildren().add(getModifierIcon(newModifier));
-          } catch (IOException e) { e.printStackTrace();
-          } catch (URISyntaxException e) { e.printStackTrace();
+            playerItems.get(gameEngine.getPlayer()).getChildren().add(getIcon(newModifier));
+          } catch (Exception e) {
+            e.printStackTrace();
           }
         }
         playerScores.get(gameEngine.getPlayer()).setText(gameEngine.getScoreAsString());
       });
-
-    }
-
-    catch (Exception e) { e.printStackTrace(); }
 
     this.players = gameEngine.getPlayers();
     for (Player player : players) {
@@ -173,7 +168,7 @@ public class PlayWindow extends GenericController {
       playerItemPane.setPadding(new Insets(5,0,15,0));
       List<Node> playerItemsList = playerItemPane.getChildren();
       player.getDice().forEach(x -> {
-        try { playerItemsList.add(getDieIcon(x)); } catch (Exception e) { e.printStackTrace(); }
+        try { playerItemsList.add(getIcon(x)); } catch (Exception e) { e.printStackTrace(); }
       });
       playerItems.put(player, playerItemPane);
 
@@ -201,7 +196,7 @@ public class PlayWindow extends GenericController {
     playerNames.get(player).setStyle("");
   }
 
-  private Group getDieIcon(Die die) throws IOException, URISyntaxException {
+  private Group getIcon(Die die) throws IOException {
     SVGPath icon = null;
     if (die.getNumOfSides() == 6) {
       icon = getSVGIcon("svgpaths/d6");
@@ -212,13 +207,12 @@ public class PlayWindow extends GenericController {
     }
     Group group = new Group();
     group.getStylesheets().add("css/playWindow.css");
-    group.setId("dies");
     group.getChildren().addAll(icon); // will return null pointer exception for weird dice
     return group;
   }
 
   // TODO modifier icon is currently all black. Should display value as well.
-  private Group getModifierIcon(Modifier mod) throws IOException, URISyntaxException {
+  private Group getIcon(Modifier mod) throws IOException {
     SVGPath icon = getSVGIcon("svgpaths/modifier");
     resizeSVG(icon, 35, 35);
     Group group = new Group();
@@ -242,6 +236,5 @@ public class PlayWindow extends GenericController {
     svg.setScaleX(width / originalWidth);
     svg.setScaleY(height / originalHeight);
   }
-
 }
 
