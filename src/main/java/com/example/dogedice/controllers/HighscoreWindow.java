d7361fb7c0c8071f1d339bb8b78330e87546b5b7
hamnord <h.nordenstein@hotmail.com>
2019-11-19 01:37:58 +0100

    added placeholders in HighscoreWindow.fxml

--- src/main/java/com/example/dogedice/controllers/HighscoreWindow.java
+++ src/main/java/com/example/dogedice/controllers/HighscoreWindow.java
@@ -1,10 +1,25 @@
 package com.example.dogedice.controllers;
 
+import javafx.fxml.FXML;
+import javafx.scene.control.Label;
 import javafx.scene.image.ImageView;
 import javafx.scene.input.MouseEvent;
+
 import java.io.IOException;
 
 public class HighscoreWindow extends GenericController {
+
+  @FXML
+  Label firstPlace, secondPlace, thirdPlace;
+
+  public void printHighscore () {
+
+    firstPlace.setText("");
+    secondPlace.setText("");
+    thirdPlace.setText("");
+    gameEngine.getHighScore();
+    gameEngine.updateHighScore();
+  }
 
   public void spinningDogeClicked(MouseEvent mouseEvent)  {
     ImageView spinningDoge = (ImageView ) mouseEvent.getSource();
@@ -20,4 +35,3 @@ public class HighscoreWindow extends GenericController {
     );
   }
 }
-
