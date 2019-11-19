61dcc842694374e95d08ec8a9b8bc7c928c65c12
Alexander <2683344+terminalnode@users.noreply.github.com>
2019-11-19 11:32:18 +0100

    updated helpwindow to read helptext from file

--- src/main/java/com/example/dogedice/controllers/HelpWindow.java
+++ src/main/java/com/example/dogedice/controllers/HelpWindow.java
@@ -3,8 +3,12 @@ package com.example.dogedice.controllers;
 import javafx.fxml.FXML;
 import javafx.scene.control.Label;
 import javafx.scene.input.MouseEvent;
+import org.apache.commons.io.IOUtils;
 
 import java.io.IOException;
+import java.nio.charset.StandardCharsets;
+
+import static java.nio.charset.StandardCharsets.UTF_8;
 
 public class HelpWindow extends GenericController {
   @FXML
@@ -20,15 +24,15 @@ public class HelpWindow extends GenericController {
 
   @FXML
   public void initialize() {
-    text.setText(
-        "1. Pick 2leg-Doge or robo Doge if less Friend.\n" +
-            "2. pick so sexi name" +
-            "3. so amaze Play! \n" +
-            "4. Play til much rich dice. \n" +
-            "5. Wow so rich, dice or mod buy now. doge impressed! \n" +
-            "6. Lose or win, u never be Top-Doge with no donation \n " +
-            "much impress Play with donation == Top-Doge" +
-            "\n No like wonder music ? M press.");
+    try {
+      text.setText(IOUtils.toString(
+          HelperMethods.getResAsStream("text/helpText"),
+          StandardCharsets.UTF_8
+      ).strip());
+      System.out.println(text.isResizable());
+    } catch (IOException e) {
+      text.setText("Could not read help text");
+    }
   }
   public void spinningDogeClicked(MouseEvent mouseEvent)  {
     HelperMethods.spinningDogeClicked(mouseEvent);
